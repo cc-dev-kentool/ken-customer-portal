ddf873c927d20c0add67d77c60c5f936ff4a814f
Nga <nga.le@codecomplete.jp>
2024-01-09 14:24:33 +0700

    Handle admin force reset password

--- src/schema/auth.tsx
+++ src/schema/auth.tsx
@@ -100,36 +100,3 @@ export const ChangePasswordValidation = Yup.object().shape({
       return result
     }),
 });
-
-// MyAccountValidation schema validates name and phone_number fields in my account form.
-export const MyAccountValidation = Yup.object().shape({
-  name: Yup.string()
-    .required("Full name is required.")
-    .max(120, "Must be less than 121 characters."),
-  phone_number: Yup.string()
-    .required("Phone number is required.")
-    .min(10, "Must be greater than 9 digits.")
-    .max(20, "Must be less than 21 digits."),
-});
-
-// ChangePasswordMyAccountValidation schema validates old_password, new_password, and reenter_password fields
-// when changing password in my account.
-export const ChangePasswordMyAccountValidation = Yup.object().shape({
-  old_password: Yup.string().required("Old password is required."),
-  new_password: Yup.string()
-    .required("New Password is required.")
-    // The password must have at least:
-    //   - one lowercase letter
-    //   - one uppercase letter
-    //   - one number
-    //   - one special character (!@#$%^&*)
-    //   - a length of 16 characters or more
-    .matches(
-      // eslint-disable-next-line
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{16,})/,
-      "Password must be at least 16 characters and contain at least three out of the following: upper case letter, lower case letter, number, and special character."
-    ),
-  reenter_password: Yup.string()
-    .required("Re-enter New Password is required.")
-    .oneOf([Yup.ref("new_password"), null], "Confirm password do not match."),
-});
